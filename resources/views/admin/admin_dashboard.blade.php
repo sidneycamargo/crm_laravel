c74a1675596e7d4fc335559a9cb16d4104816050
Sidney C Moraes <sidneycamargo@gmail.com>
2024-05-20 16:01:57 -0300

    New Seeders and update layout admin

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -66,7 +66,8 @@
 
 	<!--start switcher-->
 	<div class="switcher-wrapper">
-		<div class="switcher-btn"> <i class='bx bx-cog bx-spin'></i>
+		<!--  <div class="switcher-btn"> <i class='bx bx-cog bx-spin'></i> -->
+		<div class="switcher-btn"> <i class='bx bx-cog'></i>
 		</div>
 		<div class="switcher-body">
 			<div class="d-flex align-items-center">
