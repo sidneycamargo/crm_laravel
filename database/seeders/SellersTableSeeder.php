bfcb96362b5ff1b6978b945e595a50d7597bf65a
Sidney C Moraes <sidneycamargo@gmail.com>
2024-06-04 18:53:16 -0300

    sellers seeder

--- database/seeders/SellersTableSeeder.php
+++ database/seeders/SellersTableSeeder.php
@@ -16,16 +16,23 @@ class SellersTableSeeder extends Seeder
     {
 
         // $old_sellers = Tbl_seller_old::all();
+        $old_sellers = [
+            'sellers_id' => 1,
+            'sellers_company_id' => 1,
+            'sellers_name' => "Nome",
+            'sellers_login' => "login",
+            'sellers_email'=> "Email",
+        ];
 
         foreach ($old_sellers as $seller) {
             DB::table('tbl_sellers')->insert([
-                // 'id' => $seller->sellers_id,
-                // 'company_id' => $seller->sellers_company_id,
-                //'name' => $seller->sellers_name,
-                //'login' => $seller->sellers_login,
-                //'email' => $seller->sellers_email,
-                //'created_at' => $seller->created_at,
-                //'updated_at' => $seller->updated_at,
+                'id' => $seller->sellers_id,
+                'company_id' => $seller->sellers_company_id,
+                'name' => $seller->sellers_name,
+                'login' => $seller->sellers_login,
+                'email' => $seller->sellers_email,
+                'created_at' => $seller->created_at,
+                'updated_at' => $seller->updated_at,
                 'id' => 1,
                 'company_id' => 1,
                 'name' => 'Nome',
