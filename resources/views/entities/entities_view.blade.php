75ee94302bf79d303fce88a489bbd88fb4164ede
Sidney C Moraes <sidneycamargo@gmail.com>
2025-02-14 11:37:25 -0300

    Add View Entities and Marital Status

--- resources/views/entities/entities_view.blade.php
+++ resources/views/entities/entities_view.blade.php
@@ -70,7 +70,52 @@
     <!-- Data Table JS -->
     <script>
         $(document).ready(function() {
+
+            const DATATABLE_PTBR = {
+                "sEmptyTable": "Nenhum registro encontrado",
+                "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+                "infoEmpty": "Mostrando 0 até 0 de 0 registros",
+                "sInfoFiltered": "(Filtrados de _MAX_ registros)",
+                "sInfoPostFix": "",
+                "sInfoThousands": ".",
+                "sLengthMenu": "_MENU_ resultados por página",
+                "sLoadingRecords": "Carregando...",
+                "sProcessing": "Processando...",
+                "sZeroRecords": "Nenhum registro encontrado",
+                "sSearch": "Pesquisar",
+                "oPaginate": {
+                    "sNext": "Próximo",
+                    "sPrevious": "Anterior",
+                    "sFirst": "Primeiro",
+                    "sLast": "Último"
+                },
+                "paginate": {
+                    "first": "Primeiro",
+                    "last": "Last",
+                    "next": "Next",
+                    "previous": "Previous"
+                },
+                "oAria": {
+                    "sSortAscending": ": Ordenar colunas de forma ascendente",
+                    "sSortDescending": ": Ordenar colunas de forma descendente"
+                },
+                "select": {
+                    "rows": {
+                        "_": "Selecionado %d linhas",
+                        "0": "Nenhuma linha selecionada",
+                        "1": "Selecionado 1 linha"
+                    }
+                }
+            }
+
             var table = new DataTable('#ajaxTable', {
+                language: {
+                    info: 'Showing1 page _PAGE_ of _PAGES_',
+                    infoEmpty: 'No records available',
+                    infoFiltered: '(filtered from _MAX_ total records)',
+                    lengthMenu: 'Display _MENU_ records per page',
+                    zeroRecords: 'Nothing found - sorry'
+                }
                 layout: {
                     topStart: {
                         pageLength: {
